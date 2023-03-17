611bdf16a4302e82c6c3f2859d6ef9c55427087b
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2023-03-17 23:04:42 +0500

    first commit

---
+++ src/Tweet.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import "./App.css"
+
+function Tweet({props}) {
+    <div>
+        <h3>props.name</h3>
+        <p>props.message</p>
+    </div>
+}
+
+export default Tweet;
