8715ea816c8fb8ff523a46881caf209b5bdbfe06
Legend0300 <ahmed.mansur.bhatti@gmail.com>
2023-03-18 13:39:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import Tweet from './Tweet';
 import { useState } from 'react';
 import './App.css';
-import MovieList from './MovieList';
-import Nav from './Nav';
-import { MovieContextProvider, MovieContext } from './MovieContext';
-import AddMovie from './addMovie';
+
 function App() {  
 
   return (
-    <MovieContextProvider>
+
     <div className="app">
-      <Nav />
-      <MovieList />
-      <AddMovie />
+      THis is React router!
     </div>
-    </MovieContextProvider>
   );
 }
 export default App;
